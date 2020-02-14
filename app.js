df0b1b81054673e7a1386bd68efa197a6473f078
nettem sarath <nettem@nvestbank.com>
2020-02-14 23:44:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const cors = require('cors')
+const helmet = require('helmet')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(helmet())
+
+
+app.get('/', function(req, res){
+    res.send('HIIII prem..')
+    
+});
+
+const io = require('socket.io')(http);
+io.set('origins', '*:*');
+
+const nsp = io.of('/random');
+nsp.on('connection',(socket)=>{
+    console.log('user connected..')
+    socket.on('catcher',()=>{
+        setInterval(()=>{
+            let random_Number= getRandomInt(10) 
+            socket.emit('catcher',random_Number)
+        },10000)
+    })
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+})
+
+http.listen(port, function(){
+    console.log('listening on *:5000');
+});
